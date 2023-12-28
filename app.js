08a7ef7f74e86d5ea3f8c477b872e3dabbc91e4e
DexterLu <45140575+lu791019@users.noreply.github.com>
2023-12-28 15:08:43 +0800

    modify app.js
    modify app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 import express from 'express'
 import router from '@/router/router.js';
-
-
+import MqttFlowController from "./controller/MqttFlowController.js";
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
@@ -27,7 +25,7 @@ const options = {
             description: 'ESS Status API Server',
         },
     },
-    apis: ['app.js','./controller/*.js','./router/*.js'], // 設定為你的應用程式檔案
+    apis: ['app.js', './controller/*.js', './router/*.js'], // 設定為你的應用程式檔案
 };
 
 const specs = swaggerJsdoc(options);
@@ -36,14 +34,16 @@ app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(specs));
 
 
 
+// 啟動 MqttFlow
+const mqtt=new MqttFlowController()
+mqtt.start();
 
 
 
 
 
 
-
-app.use('/',router)
+app.use('/', router)
 
 
 
