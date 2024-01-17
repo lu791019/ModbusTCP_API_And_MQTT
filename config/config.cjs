915f5441d2d8046917e4deaf3ee53f832611850d
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-01-17 17:11:20 +0800

    add config

---
+++ config/config.cjs
@@ -0,0 +1,13 @@
+// import dotenv from 'dotenv';
+// dotenv.config(); // this is important!
+require('dotenv').config();
+module.exports = {
+    "development": {
+        "username": process.env.POSTGGRESQL_USERNAME,
+        "password": process.env.POSTGGRESQL_PASSWORD,
+        "database": process.env.POSTGGRESQL_DATABASE,
+        "host": process.env.POSTGGRESQL_HOST,
+        "port": process.env.POSTGGRESQL_PORT,
+        "dialect": "postgres"
+    },
+};
