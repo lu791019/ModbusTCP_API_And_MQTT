3eb1a923a8bb9e69877b2c946b0eb3e3588b85f4
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-01-17 17:13:17 +0800

    add moddleware

---
+++ middleware/jwtmiddleware.js
@@ -0,0 +1,26 @@
+
+import jwt from 'jsonwebtoken';
+import express from "express";
+
+
+const jwtMiddleware = async (req, res, next) => {
+    let token;
+    try {
+        token = req.headers['authorization'].split(' ')[1];
+    } catch (e) {
+        token = '';
+    }
+    jwt.verify(token,process.env.SECRET_KEY, function (err, decoded) {
+        if (err) {
+            console.log("error no jwt")
+            next();
+        } else {
+            console.log("has info")
+            // 拿出info資訊
+            // req.info = decoded;
+            console.log(decoded)
+            next();
+        }
+    });
+}
+export default jwtMiddleware;
