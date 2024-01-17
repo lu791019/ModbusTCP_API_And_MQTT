f93d218145102c435bc0c74586df35ddd3125d7b
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-01-17 17:14:09 +0800

    modified router

--- router/router.js
+++ router/router.js
@@ -3,8 +3,18 @@
 
 import express from 'express';
 const router = express.Router();
-import EssController from '@/controller/EssController.js';
 
+import jwtMiddleware from '@/middleware/jwtmiddleware.js';
+
+import EssController from '@/controller/EssController.js';
+import JWTControoler from '@/controller/JWTController.js';
+
+
+router.post('/jwt',JWTControoler.postJWT );
+
+
+// 增加middleware 判斷token
+router.use(jwtMiddleware);
 router.post('/ess_cal/ess_status', EssController.postEssStatus);
 
 
