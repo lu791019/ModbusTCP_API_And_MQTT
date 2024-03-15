6231058b793ed6191605e1391ec644313bdaef53
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-03-15 17:34:52 +0800

    Add files via upload

--- router/router.js
+++ router/router.js
@@ -1,21 +1,17 @@
 // routes.js
 
+import express from 'express'
+const router = express.Router()
 
-import express from 'express';
-const router = express.Router();
+import jwtMiddleware from '@/middleware/jwtmiddleware.js'
 
-import jwtMiddleware from '@/middleware/jwtmiddleware.js';
+import EssController from '@/controller/EssController.js'
+import JWTControoler from '@/controller/JWTController.js'
 
-import EssController from '@/controller/EssController.js';
-import JWTControoler from '@/controller/JWTController.js';
-
-
-router.post('/jwt',JWTControoler.postJWT );
-
+router.post('/jwt', JWTControoler.postJWT)
 
 // 增加middleware 判斷token
-router.use(jwtMiddleware);
-router.post('/ess_cal/ess_status', EssController.postEssStatus);
+router.use(jwtMiddleware)
+router.post('/ess_cal/ess_status', EssController.postEssStatus)
 
-
-export default router;
+export default router
