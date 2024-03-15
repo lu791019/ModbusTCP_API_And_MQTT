c8bc322874b1c5bf5e0189ced0a08eff188c5417
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-03-15 15:53:06 +0800

    Add files via upload

--- config/config.cjs
+++ config/config.cjs
@@ -1,13 +1,21 @@
 // import dotenv from 'dotenv';
 // dotenv.config(); // this is important!
-require('dotenv').config();
+require('dotenv').config()
 module.exports = {
-    "development": {
-        "username": process.env.POSTGGRESQL_USERNAME,
-        "password": process.env.POSTGGRESQL_PASSWORD,
-        "database": process.env.POSTGGRESQL_DATABASE,
-        "host": process.env.POSTGGRESQL_HOST,
-        "port": process.env.POSTGGRESQL_PORT,
-        "dialect": "postgres"
-    },
-};
+  dev: {
+    username: process.env.POSTGGRESQL_USERNAME,
+    password: process.env.POSTGGRESQL_PASSWORD,
+    database: process.env.POSTGGRESQL_DATABASE,
+    host: process.env.POSTGGRESQL_HOST,
+    port: process.env.POSTGGRESQL_PORT,
+    dialect: 'postgres',
+  },
+  prd: {
+    username: process.env.POSTGGRESQL_USERNAME,
+    password: process.env.POSTGGRESQL_PASSWORD,
+    database: process.env.POSTGGRESQL_DATABASE,
+    host: process.env.POSTGGRESQL_HOST,
+    port: process.env.POSTGGRESQL_PORT,
+    dialect: 'postgres',
+  },
+}
