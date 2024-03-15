aea093c29f6d16585bbf9be20f6c5582efdaf1ee
DexterLu <45140575+lu791019@users.noreply.github.com>
2024-03-15 17:27:51 +0800

    Add files via upload

--- influxDB/model/model/Model.js
+++ influxDB/model/model/Model.js
@@ -1,4 +1,5 @@
-import { InfluxDB, Point } from '@influxdata/influxdb-client';
+import { InfluxDB, Point } from '@influxdata/influxdb-client'
+import logger from '@/utility/logger/logger.js'
 
 /**
  * @typedef {Object} DataPoint
@@ -7,102 +8,100 @@ import { InfluxDB, Point } from '@influxdata/influxdb-client';
  * @property {Object.<string, any>} fields
  */
 
+let instance = null
 
-
-
-export default  class Model {
-
-    constructor() {
-        const URL = `http://${process.env.INFLUXDB_HOST}:${process.env.INFLUXDB_PORT}`;
-        const TOKEN = process.env.INFLUXDB_TOKEN;
-        const ORG = process.env.INFLUXDB_ORG;
-        const BUCKET = process.env.INFLUXDB_BUCKET;
-
-        // Reinitialize or update the InfluxDB instance if necessary
-        this.influxDB = new InfluxDB({
-            url: URL,
-            token: TOKEN
-        });
-        const writeOptions = {
-            batchSize: 5,        // 较小的批量大小
-            flushInterval: 2000  // 较短的刷新间隔（毫秒）
-        };
-
-        this.writeApi = this.influxDB.getWriteApi(ORG, BUCKET, 'ns',writeOptions);
-        this.measurement="";
-        this.influxDBSchema={
-            measurement: "",
-            tags: [],
-            fields: {}
-        };
-
+export default class Model {
+  constructor() {
+    if (instance) {
+      return instance
     }
 
-    setField(point, key, value) {
-        const fieldType = this.influxDBSchema.fields[key];
-        switch (fieldType) {
-            case 'integer':
-                point.intField(key, value);
-                break;
-            case 'float':
-                point.floatField(key, value);
-                break;
-            case 'boolean':
-                point.booleanField(key, value);
-                break;
-            case 'string':
-                point.stringField(key, value);
-                break;
-            // ... 處理其他類型
-            default:
-                console.error(`未知的字段類型: ${fieldType} for field ${key}`);
-                break;
-        }
+    const URL = `http://${process.env.INFLUXDB_HOST}:${process.env.INFLUXDB_PORT}`
+    const TOKEN = process.env.INFLUXDB_TOKEN
+    const ORG = process.env.INFLUXDB_ORG
+    const BUCKET = process.env.INFLUXDB_BUCKET
+
+    // Reinitialize or update the InfluxDB instance if necessary
+    this.influxDB = new InfluxDB({
+      url: URL,
+      token: TOKEN,
+    })
+    const writeOptions = {
+      batchSize: 100, // 较小的批量大小
+      flushInterval: 5000, // 较短的刷新间隔（毫秒）
     }
 
-
-
-    async  save(tag,data) {
-        // 從 data 中提取 productName 並將其設為 tag
-
-
-        const point = new Point(this.measurement );
-
-
-        Object.entries(tag).forEach(([key, value]) => {
-            point.tag(key, value);
-        })
-
-        Object.entries(data).forEach(([key, value]) => {
-
-            if (key in this.influxDBSchema.fields) {
-                this.setField(point, key, value);
-            } else {
-                console.warn(`未定義的字段: ${key}`);
-            }
-
-        });
-
-
-        return await this.writePoints(point);
+    this.writeApi = this.influxDB.getWriteApi(ORG, BUCKET, 'ns', writeOptions)
+    // this.writeApi = this.influxDB.getWriteApi(ORG, BUCKET, 'ns');
+    this.measurement = ''
+    this.influxDBSchema = {
+      measurement: '',
+      tags: [],
+      fields: {},
     }
+    instance = this
+  }
 
-
-    /**
-     * @param {DataPoint} point
-     */
-    async writePoints(point) {
-       return await this.writeApi.writePoint(point);
+  setField(point, key, value) {
+    const fieldType = this.influxDBSchema.fields[key]
+    switch (fieldType) {
+      case 'integer':
+        point.intField(key, value)
+        break
+      case 'float':
+        point.floatField(key, value)
+        break
+      case 'boolean':
+        point.booleanField(key, value)
+        break
+      case 'string':
+        point.stringField(key, value)
+        break
+      // ... 處理其他類型
+      default:
+        console.error(`未知的字段類型: ${fieldType} for field ${key}`)
+        break
     }
+  }
 
-    async close() {
-        try {
-            await this.writeApi.close();
-            console.log('Flushed data and closed write API');
-        } catch (e) {
-            console.error('Error closing write API:', e);
-        }
+  async save(tag, data) {
+    // 從 data 中提取 productName 並將其設為 tag
+
+    const point = new Point(this.measurement)
+
+    Object.entries(tag).forEach(([key, value]) => {
+      point.tag(key, value)
+    })
+
+    Object.entries(data).forEach(([key, value]) => {
+      if (key in this.influxDBSchema.fields) {
+        this.setField(point, key, value)
+      } else {
+        console.warn(`未定義的字段: ${key}`)
+      }
+    })
+
+    return await this.writePoints(point)
+  }
+
+  /**
+   * @param {DataPoint} point
+   */
+  async writePoints(point) {
+    try {
+      await this.writeApi.writePoint(point)
+    } catch (e) {
+      logger.error(e)
     }
+    return true
+  }
 
-
+  async close() {
+    try {
+      await this.writeApi.close()
+      console.log('Flushed data and closed write API')
+    } catch (e) {
+      console.error('Error closing write API:', e)
+    }
+  }
 }
--- influxDB/model/model/SkysailsInfluxModel.js
+++ influxDB/model/model/SkysailsInfluxModel.js
@@ -1,35 +1,26 @@
+import Model from '@/influxDB/model/Model.js'
 
-import Model  from "@/influxDB/model/Model.js";
-
-export default  class SkysailsInfluxModel extends Model{
-//    複寫父類別的方法
-    constructor() {
-        super();
-        this.measurement = 'skysails';
-        this.influxDBSchema = {
-            measurement: this.measurement,
-            tags: ['productName'],
-            fields: {
-                // 'Power_Chopper1_kW':'float',// 斬波功率
-                // 'McAppWindSpeedMast_m_s':'float',// 桅杆風速計風速
-                // 'WsAirDensityGround_kg_m3':'float',// 空氣密度
-                // 'WsAirPressureGround_hPa':'float',// 大氣壓力
-                // 'WsAirTemperatureGround_degC':'float',// 溫度
-                // 'WsAppWindSpeedGround_m_s':'float',// 地表風速
-                // 'WsDewPointGround_degC':'float',// 露點
-                // 'WsGlobalRadiation_W_m2':'float',// 全輻射
-                // 'WsHumidityGround_percent':'float',// 濕度
-                // 'McAppWindAngleMast_deg':'float',// 桅杆風速計風向
-                // 'MsAppWindAngleGround_deg':'float',// 地表風向
-            }
-        };
-
-
+export default class SkysailsInfluxModel extends Model {
+  //    複寫父類別的方法
+  constructor() {
+    super()
+    this.measurement = 'skysails'
+    this.influxDBSchema = {
+      measurement: this.measurement,
+      tags: ['productName'],
+      fields: {
+        // 'Power_Chopper1_kW':'float',// 斬波功率
+        // 'McAppWindSpeedMast_m_s':'float',// 桅杆風速計風速
+        // 'WsAirDensityGround_kg_m3':'float',// 空氣密度
+        // 'WsAirPressureGround_hPa':'float',// 大氣壓力
+        // 'WsAirTemperatureGround_degC':'float',// 溫度
+        // 'WsAppWindSpeedGround_m_s':'float',// 地表風速
+        // 'WsDewPointGround_degC':'float',// 露點
+        // 'WsGlobalRadiation_W_m2':'float',// 全輻射
+        // 'WsHumidityGround_percent':'float',// 濕度
+        // 'McAppWindAngleMast_deg':'float',// 桅杆風速計風向
+        // 'MsAppWindAngleGround_deg':'float',// 地表風向
+      },
     }
-
-
-
-
-
-
+  }
 }
